ca8aed7bee3395824d53f23e13961fbafae56965
Dennis Smink <dennissmink@gmail.com>
2016-01-16 21:29:12 +0100

    wauw

--- src/test.php
+++ src/test.php
@@ -3,9 +3,3 @@
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
 use LaravelFtp\Ftp;
-
-$ftp = new Ftp('80.82.209.146', 'Dennis.17413', 'BOzAtjDx98');
-
-echo '<pre>';
-
-print_r($ftp->all());
