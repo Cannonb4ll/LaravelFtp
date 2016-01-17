1abaa34af90f90342c066974af0cab57a4697113
Dennis Smink <dennissmink@gmail.com>
2016-01-17 08:27:25 +0100

    Collection implemented, improved ftp methods

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Laravel FTP wrapper for Laravel (or any other project that has composer)
 
 This project is just called LaravelFtp because I am holding this package to the conventions Laravel itself uses, they are great and easy.
 
+It also utilises the collection package, files are returned as collection so you can use the Collection function from Laravel.
+
 # Usage
 
 ## Installation
@@ -33,7 +35,22 @@ if($ftp){
 
 **Methods**
 
+*** Display files ***
 ```
 $ftp->all(); // Returns all the files of the users root files
 $ftp->all('folder'); // Returns all the files of the directory folder
+
+$ftp->get('filename.txt') // Returns the content of the file, can also be like: get('directory/filename.txt')
+
+```
+
+
+*** Create files/directories ***
+```
+$ftp->createDirectory('directory name');
+```
+
+*** Delete files/directories ***
+```
+$ftp->deleteDirectory('directory name');
 ```
--- src/LaravelFtp/Ftp.php
+++ src/LaravelFtp/Ftp.php
@@ -2,6 +2,8 @@
 
 namespace LaravelFtp;
 
+use Illuminate\Support\Collection;
+
 class FTP
 {
     private $connection;
@@ -10,16 +12,16 @@ class FTP
      * public function __construct
      *
      *
-     * @param    string    $host
-     * @param    string    $user
-     * @param    string    $pass
+     * @param    string $host
+     * @param    string $user
+     * @param    string $pass
      * @return    boolean
      */
     public function __construct($host, $user, $pass, $port = 21)
     {
-        if($this->connection = @ftp_connect($host, ($port != 21 )? $port : 21)){
-            if( @ftp_login( $this->connection, $user, $pass )){
-                ftp_pasv( $this->connection, true );
+        if ($this->connection = @ftp_connect($host, ($port != 21) ? $port : 21)) {
+            if (@ftp_login($this->connection, $user, $pass)) {
+                ftp_pasv($this->connection, true);
                 return true;
             }
         }
@@ -27,40 +29,43 @@ class FTP
     }
 
     /**
-     * public function ls
+     * public function all
      *
      *
-     * @param    string    $directory
+     * @param    string $directory
      * @return   string
      */
-    public function all( $directory = '' ){
-        return ftp_nlist( $this->connection, $directory );
+    public function all($directory = '')
+    {
+        return collect(ftp_nlist($this->connection, $directory));
     }
 
     /**
      * public function size
      *
      *
-     * @param    string    $file
+     * @param    string $file
      * @return   string
      */
-    public function size( $file = '' ){
-        return @ftp_size( $this->connection, $file );
+    public function size($file = '')
+    {
+        return @ftp_size($this->connection, $file);
     }
 
     /**
      * public function get
      *
      *
-     * @param    string    $file
+     * @param    string $file
      * @return   string
      */
-    public function get( $file = '' ){
+    public function get($file = '')
+    {
         $tempHandle = fopen('php://temp', 'r+');
-        $sizeFile = $this->size( $file );
-        if( $sizeFile > 512000 ){ // 512 000 KB
-            return "This file is too big to read, maximum filesize allowed to the browser: 512KB";
-        }else{
+        $sizeFile = $this->size($file);
+        if ($sizeFile > 512000) { // 512 000 KB
+            return 'This file is too big to read, maximum filesize allowed to the browser: 512KB';
+        } else {
             if (@ftp_fget($this->connection, $tempHandle, $file, FTP_ASCII, 0)) {
                 rewind($tempHandle);
                 $total = stream_get_contents($tempHandle);
@@ -72,15 +77,25 @@ class FTP
         return false;
     }
 
-    public function deleteFile( $pathFile ){
-        return @ftp_delete($this->connection, str_replace("../", "", $pathFile ) );
+    public function createFile($directory, $name)
+    {
+        $temp = tmpfile();
+
+        return @ftp_fput($this->connection, $directory . '/' . $name, $temp, FTP_ASCII);
     }
 
-    public function createDirectory( $directory ){
-        return @ftp_mkdir( $this->connection, $directory );
+    public function deleteFile($file)
+    {
+        return @ftp_delete($this->connection, str_replace('../', '', $file));
     }
 
-    public function removeDirectory( $directory ){
-        return @ftp_rmdir($this->connection, $directory );
+    public function createDirectory($directory)
+    {
+        return @ftp_mkdir($this->connection, $directory);
+    }
+
+    public function deleteDirectory($directory)
+    {
+        return @ftp_rmdir($this->connection, $directory);
     }
 }
